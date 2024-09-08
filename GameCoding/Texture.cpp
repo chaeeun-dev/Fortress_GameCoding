eb26085fe599b87eefa98c3af42c22e8e800023b
im-chaeeun <dev.chaeng@gmail.com>
2024-09-08 19:08:37 +0900

    오류가 뭘까.....

--- GameCoding/Texture.cpp
+++ GameCoding/Texture.cpp
@@ -15,8 +15,7 @@ Texture* Texture::LoadBmp(HWND hwnd, const wstring& path)
 
 	// 이미지 로드는 개념적으로 크게 중요한 건 아니니 깊게 생각하진 말자
 	_hdc = ::CreateCompatibleDC(hdc);
-	_bitmap = (HBITMAP)::LoadImage(nullptr, path.c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-	
+	_bitmap = (HBITMAP)::LoadImage(nullptr, path.c_str(), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
 	if (_bitmap == 0)		// 이미지가 없으면 경고 메세지
 		::MessageBox(hwnd, path.c_str(), L"Image Load Failed", NULL);
 
